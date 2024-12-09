48caf8676051d6915cd05d2f0fa0ef74cfad7316
soodala2003 <soodala78@gmail.com>
2024-12-09 01:59:58 -0500

    add three pages

---
+++ src/pages/Character.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import CharacterCard from "../components/CharacterCard";
+import "../styles/Character.css";
+
+export default function Character () {
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const baseURL = "https://rickandmortyapi.com/api/";
+    const getPaginatePage = async () => {
+      try {
+        const response = await fetch(`${baseURL}character/?page=${page + 1}`);
+        const result = await response.json();
+        const data = result.results;
+        setCharacters(data);
+        console.log(data[0]);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getPaginatePage();
+  }, [page]);
+
+  function handlePrevPage() {
+    if (page <= 0) {
+      setPage(0);
+    } else {
+      setPage((prev) => prev - 1);
+    }
+  }
+
+  function handleNextPage() {
+    if (page >= 41) {
+      setPage(0);
+    } else {
+      setPage((prev) => prev + 1);
+    }
+  }
+
+  // loaded function for when data is fetched.
+  const loaded = () => {
+    return (
+      <div>
+        <div className="pagination">
+          <button className="btn" onClick={handlePrevPage}>
+            PREV
+          </button>
+          <p className="my-auto">{page + 1} OF 42 </p>
+          <button className="btn" onClick={handleNextPage}>
+            NEXT
+          </button>
+        </div>
+        <div className="characters">
+        {characters.map((character, index) => {
+          return (
+            <li key={index}>
+              <div className="character-list">
+                <CharacterCard key={character.id} character={character} />
+              </div>
+            </li>
+          );
+        })}   
+        </div>
+      </div>
+    );
+  };
+
+  // Function for when data doesn't exist.
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  // If coin has data, run the loaded function; otherwise, run loading.
+  return characters ? loaded() : loading();
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import "../styles/Home.css";
+import Rick_and_Morty_season_1 from "../images/Rick_and_Morty_season_1.png";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="season-list">
+        <h1>Rick and Morty</h1>
+        <h2>Season 1</h2>
+        <img className="season-1-img" src={Rick_and_Morty_season_1} alt="Rick_and_Morty_season_1" />
+      </div> 
+    </div>
+  );
+}
+
+export default Home;
