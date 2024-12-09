71a18f45cf327c2a5ef927ed16d2702f7e009854
soodala2003 <soodala78@gmail.com>
2024-12-09 01:45:47 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Character from "./pages/Character";
+import CharacterDetail from "./pages/CharacterDetail";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} /> 
+        <Route exact path="/character" element={<Character />} /> 
+        <Route exact path="/character/:id" element={<CharacterDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
