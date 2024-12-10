1e31bb829e65003c641da71d1ef43e6d1a04f766
soodala2003 <soodala78@gmail.com>
2024-12-10 10:49:53 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Character from "./pages/Character";
