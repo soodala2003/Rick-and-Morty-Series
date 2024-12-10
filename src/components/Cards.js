c762f714ff1cbebebd94e626a1ba46cafc9e9e2a
soodala2003 <soodala78@gmail.com>
2024-12-10 11:10:56 -0500

    update components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Cards.css";
 
+
 export const CharacterCard = ({ character }) => {
   return (
     <div className="character-card">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
+
 const Navbar = () => {
   return (
     <nav className="navbar">
